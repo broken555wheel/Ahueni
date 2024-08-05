58414a02599b8ad84829c5d594d5708e31eb9b1a
David Nzambuli <nzambulidavid@gmail.com>
2024-08-05 15:54:38 +0300

    Added most components of the homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:ahueni/components/my_app_bar.dart';
 import 'package:ahueni/components/my_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,9 +13,116 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(title: 'Home'),
-      drawer: MyDrawer(),
+    return Scaffold(
+      appBar: const MyAppBar(title: 'Home'),
+      drawer: const MyDrawer(),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Overcoming',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontSize: 20),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    'Cocaine',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                'Sober Start Date:',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 20),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            widthFactor: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(100)),
+              width: 290,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Center(
+                  widthFactor: 1,
+                  child: Text(
+                    'Sober Start Date:',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                'Sober For:',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 20),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(200),
+                ),
+                child: Center(
+                  widthFactor: 1,
+                  child: Text(
+                    'Sober For:',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.surface,
+                        fontSize: 20),
+                  ),
+                )),
+          )
+        ],
+      ),
     );
   }
 }
